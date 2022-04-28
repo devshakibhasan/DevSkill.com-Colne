5b56c47ffd8d54ccaec167d7191e7393fd433c53
shakib hasan <shakibhasan@gmail.com>
2022-04-28 09:34:38 +0600

    create courses section

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button, Card } from 'react-bootstrap';
 import NavbarCom from '../Navbar/NavbarCom';
 import './Home.css'
 export default class Home extends Component {
@@ -40,18 +40,48 @@ export default class Home extends Component {
                   No need to waste time in traffic jam. A good internet  connection is all you need to start learning!
                 </p>
               </Col>
-             
+
             </Row>
 
             <Row className='skills'>
               <Col>
-              <h4>Over 13935 students from multiple renowned universities and institutions across the country attended courses/contests </h4>
+                <h4>Over 13935 students from multiple renowned universities and institutions across the country attended courses/contests </h4>
               </Col>
               <Col>
-              <img src='https://devskill.com/img/students.png'></img>
+                <img src='https://devskill.com/img/students.png'></img>
               </Col>
             </Row>
+            <Container className='courses-body'>
+              <h1>Upcoming</h1>
+              <Card style={{ width: '36rem', height: '36rem'
+             }}>
+
+                <Card.Body className='card'>
+                  <Row>
+                    <Col className='course-left' >
+                      <img src='https://devskillpublicfiles.s3.ap-southeast-1.amazonaws.com/uploads/websiteimages/coursecontents/21xwzm0jtfj.png'></img>
+                    </Col>
+                    <Col >
+                    <h5> UI and UX Design for APP and Website </h5>
+                    <div></div>
+                    <p>This course has been designed with effective practical components that makes you a professional/ commercial UI designer. From the Information architecture to Sketch to Wireframe to prototype and UI de... </p>
+                    </Col>
+                  </Row>
+
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+
+
+          
+            </Container>
+
           </Container>
+
+        </div>
+        <div className='courses'>
+          <img src='https://devskill.com/img/bg_upcoming_left.svg'></img>
+
         </div>
 
       </div>
